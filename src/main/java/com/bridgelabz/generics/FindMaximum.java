c00fc1b90bedeb9616a6b61afb93acd3e0e68990
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-01-22 17:04:15 +0530

    Refactored : Create Generic Class with Parameter Constructor

--- src/main/java/com/bridgelabz/generics/FindMaximum.java
+++ src/main/java/com/bridgelabz/generics/FindMaximum.java
@@ -2,9 +2,20 @@ package com.bridgelabz.generics;
 
 import java.util.Scanner;
 
-public class FindMaximum {
+public class FindMaximum<T extends Comparable<T>> {
+	
+	T value1;
+	T value2;
+	T value3;
+	
+	public FindMaximum(T value1,T value2,T value3){
+		
+		this.value1 = value1;
+		this.value2 = value2;
+		this.value3 = value3;
+	}
 
-	public static <T extends Comparable<T>> void Maximum(T value1, T value2, T value3) {
+	public <T extends Comparable<T>> void Maximum(T value1, T value2, T value3) {
 		T max = value1;
 		if(value1.compareTo(max)>0 && value2.compareTo(value3)>0)
 			System.out.println(value2+" is Maximum");
@@ -26,7 +37,7 @@ public class FindMaximum {
 		
 		System.out.print("Enter the Third Number : ");
 		Integer value3 = scan.nextInt();
-		
-		Maximum(value1, value2, value3);
+		FindMaximum<Integer> integers = new FindMaximum<Integer>(value1, value2, value3);
+		integers.Maximum(value1, value2, value3);		
 	}
 }
